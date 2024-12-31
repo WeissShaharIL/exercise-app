ae0ba65165f422baa9eb513179e1d9b235a334b4
Shahar Weiss <weiss.shahar.il@gmail.com>
2024-12-31 08:36:19 +0200

    Add Init Screen feature and update Known Issues (#4)

---
+++ app/src/main/java/com/example/exerciseapp/view/components/LoadingScreen.kt
@@ -0,0 +1,23 @@
+package com.example.exerciseapp.view.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.material3.MaterialTheme
+
+@Composable
+fun LoadingScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Loading...",
+            style = MaterialTheme.typography.bodyLarge,
+            color = MaterialTheme.colorScheme.primary
+        )
+    }
+}
